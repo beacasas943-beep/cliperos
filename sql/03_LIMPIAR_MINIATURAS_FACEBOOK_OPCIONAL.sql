802e657f7bb1cce01d45c567e1bb28a2611075ce
beacasas943-beep <beacasas943@gmail.com>
2026-08-15 19:53:07 -0500

    Delete sql directory

--- sql/03_LIMPIAR_MINIATURAS_FACEBOOK_OPCIONAL.sql
+++
@@ -1,8 +0,0 @@
--- OPCIONAL: úsalo solo si quieres quitar de la BD las miniaturas temporales de Facebook.
--- No toca miniaturas ya copiadas a Supabase Storage.
-update public.videos
-set thumbnail_url = null
-where lower(platform) = 'facebook'
-  and deleted_at is null
-  and thumbnail_url is not null
-  and (thumbnail_url ilike '%fbcdn.net%' or thumbnail_url ilike '%fbsbx.com%');
